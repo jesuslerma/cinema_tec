959fe5ca0fe31f5a0fb19cb9420283ce5aa6abdd
jesus lerma sanchez <shuyojl@gmail.com>
2016-04-12 22:04:32 -0500

    add test movie has rankings and passing

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,5 @@
 class Movie < ActiveRecord::Base
   validates :name, presence: true
+  validates :name, uniqueness: true
+  has_many :ranking
 end
--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,8 +1,26 @@
 require 'test_helper'
 
 class MovieTest < ActiveSupport::TestCase
+  # test 'movie without name must be invalid' do
+  #   m = Movie.new name:nil
+  #   assert_not m.valid?
+  # end
+  def setup
+    @movie = movies(:batman_vs_superman)
+  end
   test 'movie without name must be invalid' do
-    m = Movie.new name:nil
-    assert_not m.valid?
+    m = movies(:batman_vs_superman)
+    m.name = nil
+    assert_not m.valid?, 'A new movie must have a name'
+  end
+
+  test 'movie with repeated name must be invalid' do
+    Movie.create(name: "Piratas de Silicon Valley")
+    m = Movie.new(name:"Piratas de Silicon Valley")
+    assert_not m.valid?, 'Movie name can\'t be repeated'
+  end
+
+  test "movie has rankings" do    
+    assert_includes @movie.ranking, rankings(:one), "Batman vs superman movie should have one ranking"
   end
 end
