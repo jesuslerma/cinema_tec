f6cc5794a34ce9b45ab48e3c7a90039b3925bdde
jesus lerma sanchez <shuyojl@gmail.com>
2016-04-12 22:05:22 -0500

    running ranking migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160413021629) do
+ActiveRecord::Schema.define(version: 20160413023907) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "name"
@@ -19,5 +19,14 @@ ActiveRecord::Schema.define(version: 20160413021629) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "rankings", force: :cascade do |t|
+    t.integer  "stars"
+    t.integer  "movie_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "rankings", ["movie_id"], name: "index_rankings_on_movie_id"
 
 end
